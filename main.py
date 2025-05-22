42696c706e4046eeed3aa09361d4005baf5b5558
Ali Torabi <alithema@gmail.com>
2025-05-22 12:05:14 -0600

    Fix issues

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import upload
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
+    # allow_origins=["http://localhost:3000"],
     allow_origins=[
         "https://unrivaled-paletas-19b54d.netlify.app",
         "https://www.unrivaled-paletas-19b54d.netlify.app"
