503a1a7413b497642e6b71830bcf2dc5e23d7a7f
Ali Torabi <alithema@gmail.com>
2025-06-02 16:06:07 -0600

    New capabilities - Cross Section

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:3000"],
-    allow_origins=[
-        "https://unrivaled-paletas-19b54d.netlify.app",
-        "https://www.unrivaled-paletas-19b54d.netlify.app"
-    ],
+    allow_origins=["http://localhost:3000"],
+    # allow_origins=[
+    #     "https://unrivaled-paletas-19b54d.netlify.app",
+    #     "https://www.unrivaled-paletas-19b54d.netlify.app"
+    # ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
