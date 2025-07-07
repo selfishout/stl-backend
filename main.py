47130b977de453055238b6610cdb604f2d0f7c5c
Ali Torabi <alithema@gmail.com>
2025-07-06 19:33:37 -0600

    Lots of files need to be changed.

--- main.py
+++ main.py
@@ -14,11 +14,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://unrivaled-paletas-19b54d.netlify.app",
-        "https://www.unrivaled-paletas-19b54d.netlify.app"
-    ],
+    allow_origins=["*"],  # Allow all origins temporarily for testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
